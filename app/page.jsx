fd049681e2da26e0fe05fd63a3d7eb6613a668af
Justice Udehegbunam <140954375+Justice-Udehegbunam@users.noreply.github.com>
2024-09-16 18:15:57 +0100

    Project file set-up

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <section className="w-full justify-start flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptoptia is an open-source AI prompting tool for modern world to
+        discover create and share creative prompts
+      </p>
+
+      {/* Feed section */}
+    </section>
+  );
+};
+
+export default Home;
